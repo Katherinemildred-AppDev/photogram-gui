9774788ec5a953edf96877ba6fb9c53cadea4986
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-05-01 21:22:58 +0000

    most of read action is looking good still need to show comments

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,4 +7,18 @@ class PhotosController < ApplicationController
     render({:template => "photo_templates/index.html.erb"})
 
   end
+
+  def show
+
+    #params : {path id=> "777"}
+
+    url_id = params.fetch("path_photo")
+
+    matching_photos= Photo.where({:id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    render({:template=> "photo_templates/show.html.erb"})
+  end
+
 end
