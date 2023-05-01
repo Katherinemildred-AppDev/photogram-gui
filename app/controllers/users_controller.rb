68dbea84e64ef60b92e65323f991454b361ef5cf
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-05-01 20:08:33 +0000

    individual user page coming together

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,25 @@ def index
 
 end
 
+def show
+
+  url_username = params.fetch("path_username")
+  
+  matching_usernames = User.where({ :username => url_username})
+
+  @the_user = matching_usernames.at(0)
+
+
+  #for reference, should send people a 404 or if then for nil
+ # if the_user == nil
+     # redirect_to ("/404") or ("/users"), etc. 
+  #else
+   
+  render({:template=>"user_templates/show.html.erb"})
+
+   # end (if we were to do the if then for nil)
+
+
+end
+
 end
