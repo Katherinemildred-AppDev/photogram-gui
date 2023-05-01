0f5ad1676cdc6283a4924e5d6f58247bded097b4
Katherine O'Connell <koconnel@chicagobooth.edu>
2023-05-01 23:12:24 +0000

    able to update images!!

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -52,7 +52,34 @@ class PhotosController < ApplicationController
    # render ({:template=> "photo_templates/create.html.erb"})
 
   redirect_to("/photos/")
+  end
+
+  def update
+    #https://3000-katherinemi-photogramgu-bg5jch1un6w.ws-us96b.gitpod.io/update_photo/957?query_image=https%3A%2F%2Fwww.chicagobooth.edu%2F-%2Fmedia%2Fproject%2Fchicago-booth%2Fwhy-booth%2Fstory-hub%2Fnew-stories-2023%2Ffebruary-2023%2Fchicago-booth-leaders-of-color.jpg%3Fcx%3D0.42%26cy%3D0.43%26cw%3D940%26ch%3D749%26hash%3DD8012F8D8774FF3DCA39E2B4459E62BE&query_caption=Updated+caption%21
+    #query_image
+    #query_caption
+  
+    the_id = params.fetch ("path_modify")
+
+    matching_photos = Photo.where({:id=> the_id})
+
+    the_photo = matching_photos.at(0)
+
+    input_image = params.fetch("query_image")
+    input_caption= params.fetch("query_caption")
+   # input_owner_id= params.fetch("query_owner_id", "howdy") #howdy is a fallback action. but no input id here.
+
+   the_photo.image = input_image
+   the_photo.caption= input_caption
+
+   the_photo.save
+
+   #render({:template=>"photo_templates/update.html.erb"})
+
+   next_url = "/photos/" +the_photo.id.to_s
+   redirect_to(next_url)
 
   end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ get("/delete_photo/:path_id", {:controller=>"photos", :action=>"delete"})
 
 get("/insert_photo", {:controller=>"photos", :action=>"create"})
 
+get("/update_photo/:path_modify", {:controller=>"photos", :action=>"update"})
+
 end
